8cbcc308bfed6ced287a3b905c4fe1b1a2cc1802
Aya Ramirez <ayaramz@gmail.com>
2022-06-10 00:03:43 -0400

    commit tree f9b6d7d15a30720700546491866037b937c70168

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,48 @@
 def admin_login(username, password)
-  # your code here
+  if username == "ADMIN" || username == "admin" && password == "12345"
+    "Access granted"
+  else 
+    "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  case temperature
+  when 10..40
+    "It's brisk out there!"
+  when 40..65 
+    "It's a little chilly out there!"
+  when 85..100
+    "It's too dang hot out there!"
+  else 
+    "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+ if num % 3 == 0 && num % 5 == 0
+  "FizzBuzz"
+ elsif num % 3 == 0 
+  "Fizz"
+ elsif num % 5 == 0
+  "Buzz"
+ else
+   num
+ end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when "+"
+    num1 + num2
+  when "-"
+    num1 - num2
+  when "*"
+    num1 * num2
+  when "/"
+    num1 / num2
+  else 
+    puts "Invalid operation!"
+  end
 end
 
